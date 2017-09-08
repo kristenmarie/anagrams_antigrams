a8f6230466aae9da935ff1860087fdf4ab6b83ef
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-08 09:27:52 -0700

    check if anagrams are also palindromes

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -6,9 +6,21 @@ class AnagramAntigram
 
   def anagram(phrase2)
     if (@phrase.downcase.split("").sort.join == phrase2.downcase.split("").sort.join)
-      return "These words are anagrams"
+      if(is_palindrome?(@phrase, phrase2))
+        return "These words are anagrams AND palindromes!"
+      else
+        return "These words are anagrams"
+      end
     else
       return "These words are not anagrams"
+    end
+  end
+
+  def is_palindrome?(phrase1, phrase2)
+    if(phrase1.reverse == phrase2)
+      return true
+    else
+      return false
     end
   end
 end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -7,8 +7,13 @@ describe("Phrase#anagram") do
     expect(test.anagram("eat")).to(eq("These words are anagrams"))
   end
 
-  it("returns words are anagrams reguardless of case") do
+  it("returns true if words are anagrams reguardless of case") do
     test = AnagramAntigram.new("Tea")
     expect(test.anagram("Eat")).to(eq("These words are anagrams"))
+  end
+
+  it("checks to see if confirmed anagrams are also palindromes") do
+    test = AnagramAntigram.new("racecar")
+    expect(test.anagram("racecar")).to(eq("These words are anagrams AND palindromes!"))
   end
 end
